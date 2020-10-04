a4a8f0d33ff3203de15dec0bdcb4facadc61e9a2
Grant Henderson <grant_github@grantph.com>
2020-10-04 12:52:56 +0000

    Added FormGroup and Option controls.

---
+++ Blazorade.Bootstrap.Forms/FormGroup.razor.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Blazorade.Bootstrap.Forms
+{
+	public partial class FormGroup
+	{
+		[Parameter] public string Label { get; set; } = string.Empty;
+
+		public FormGroup()
+		{
+		}
+
+		// Debugging.
+		//private static int count = 0;
+
+		protected override async Task OnParametersSetAsync()
+		{
+			// Debugging. Sometimes it's nice to know which FormGroup we are in when debugging errors. It might be nice to build this in somehow, as it can help save debug time.
+			//Console.WriteLine($"{nameof(FormGroup)} {count++}");
+
+			// Add FormGroup Class
+			AddClasses("form-group");
+
+			await base.OnParametersSetAsync();
+		}
+	}
+}
---
+++ Blazorade.Bootstrap.Forms/Option.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazorade.Bootstrap.Forms
+{
+	public partial class Option
+	{
+		[Parameter] public string Name { get; set; }
+		[Parameter] public string Value { get; set; }
+		[Parameter] public bool Selected { get; set; }
+
+		protected override void OnParametersSet()
+		{
+			if (!string.IsNullOrEmpty(Value))
+			{
+				AddAttribute("value", Value);
+			}
+
+			if (Selected)
+			{
+				AddAttribute("selected", string.Empty);
+			}
+
+			base.OnParametersSet();
+		}
+	}
+}
