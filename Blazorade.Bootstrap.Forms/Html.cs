8525bd2274b175639b1065b6cb473b31b392e711
Grant Henderson <grant_github@grantph.com>
2020-10-04 08:54:48 +0000

    Started Html constants file to improve code quality.

---
+++ Blazorade.Bootstrap.Forms/Html.cs
@@ -0,0 +1,18 @@
+﻿namespace Blazorade.Bootstrap.Forms
+{
+	public static partial class Html
+	{
+		public const string INPUT = "input";
+		public const string TEXTAREA = "textarea";
+
+		public const string CLASS = "class";
+		public const string DISABLED = "disabled";
+		public const string VALUE = "value";
+
+		public const string ONBLUR = "onblur";
+		public const string ONCHANGE = "onchange";
+		public const string ONCLICK = "onclick";
+
+		public const string ARIA_DESCRIBEDBY = "aria-describedby";
+	}
+}
