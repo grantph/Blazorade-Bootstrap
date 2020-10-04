040c3228c0569253170b0d23b94bbe2dc80c1cb2
Grant Henderson <grant_github@grantph.com>
2020-10-04 23:20:20 +0000

    Added Bootstrap constants.

---
+++ Blazorade.Bootstrap.Forms/Bootstrap.cs
@@ -0,0 +1,30 @@
+﻿namespace Blazorade.Bootstrap.Forms
+{
+	public static class Bootstrap
+	{
+		public const string CHECKBOX_INLINE = "checkbox-inline";
+
+		public const string FORM_CHECK = "form-check";
+		public const string FORM_CHECK_INPUT = "form-check-input";
+		public const string FORM_CHECK_LABEL = "form-check-label";
+
+		public const string FORM_CONTROL = "form-control";
+		public const string FORM_CONTROL_LARGE = "form-control-lg";
+		public const string FORM_CONTROL_SMALL = "form-control-sm";
+
+		public const string FORM_GROUP = "form-group";
+
+		public const string FORM_TEXT = "form-text";
+
+		public const string HELP = "help";
+
+		public const string INPUT_GROUP = "input-group";
+		public const string INPUT_GROUP_PREPEND = "input-group-prepend";
+		public const string INPUT_GROUP_TEXT = "input-group-text";
+
+		public const string LABEL_LARGE = "label-lg";
+		public const string LABEL_SMALL = "label-sm";
+
+		public const string TEXT_MUTED = "text-muted";
+	}
+}
