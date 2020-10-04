65de60f863820dfe26c291055a78013c6b5ecbd4
Grant Henderson <grant_github@grantph.com>
2020-10-04 12:51:30 +0000

    Created initial showroom controls for Text, TextArea, and TextBox controls.

---
+++ Blazorade.Bootstrap.Components.Showroom/Forms/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using Blazorade.Bootstrap.Forms;
+
+namespace Blazorade.Bootstrap.Components.Showroom.Forms.Models
+{
+	internal interface IAddress : IModelState
+	{
+		string Street { get; set; }
+	}
+
+	public class Address : IAddress
+	{
+		public ModelState ModelState { get; set; } = ModelState.Edit;
+
+		public string Street { get; set; }
+	}
+}
