19f750853d9ba1311d725a3827908ac3fa9f45e6
Grant Henderson <grant_github@grantph.com>
2020-10-04 23:21:21 +0000

    Implemented Select, and Checkbox examples.

---
+++ Blazorade.Bootstrap.Components.Showroom/Forms/Models/CheckboxExample.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Blazorade.Bootstrap.Forms;
+
+namespace Blazorade.Bootstrap.Components.Showroom.Forms.Models
+{
+	public class CheckboxExample : IModelState
+	{
+		public ModelState ModelState { get; set; } = ModelState.Edit;
+
+		public bool Example1 { get; set; } = false;
+
+		[Required(ErrorMessage = "Example 2 is required")]
+		public bool Example2 { get; set; } = true;
+
+		public bool Example3 { get; set; } = false;
+
+		public bool Example4 { get; set; } = true;
+
+		public bool Example5 { get; set; } = false;
+
+		[Required(ErrorMessage = "Example 6 is required")]
+		public bool Example6 { get; set; } = true;
+	}
+}
---
+++ Blazorade.Bootstrap.Components.Showroom/Forms/Models/SelectExample.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Blazorade.Bootstrap.Forms;
+
+namespace Blazorade.Bootstrap.Components.Showroom.Forms.Models
+{
+	public class SelectExample : IModelState
+	{
+		public ModelState ModelState { get; set; } = ModelState.Edit;
+
+		public string Example1 { get; set; } = "Anything you want...";
+
+		[Required(ErrorMessage = "Example 2 is required")]
+		public string Example2 { get; set; } = string.Empty;
+
+		[MinLength(10, ErrorMessage = "Example 3 requires at least 10 characters")]
+		public string Example3 { get; set; } = "1234567";
+
+		[MaxLength(5, ErrorMessage = "Example 4 max 4 characters")]
+		public string Example4 { get; set; } = "1234567890";
+
+		[Required]
+		public int? Example5 { get; set; } = null;
+
+		[Required]
+		public double Example6 { get; set; } = 1.3;
+	}
+}
