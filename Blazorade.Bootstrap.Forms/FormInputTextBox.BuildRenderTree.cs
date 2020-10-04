e38ac9754628f41707f36f3f608f3c48d07c81bf
Grant Henderson <grant_github@grantph.com>
2020-10-04 08:56:54 +0000

    Replaced strings with Html constants. Fixed ClearOnClick and SelectOnClick implementations.

--- Blazorade.Bootstrap.Forms/FormInputTextBox.BuildRenderTree.cs
+++ Blazorade.Bootstrap.Forms/FormInputTextBox.BuildRenderTree.cs
@@ -20,26 +20,32 @@ namespace Blazorade.Bootstrap.Forms
 				case TextBoxMode.SingleLine:
 					{
 						// Input
-						builder.OpenElement(0, "input");
+						builder.OpenElement(0, Html.INPUT);
 						builder.AddMultipleAttributes(1, this.Attributes);
-						builder.AddAttribute(2, "class", CssClass); // Overwrite Attributes["class"]
-						builder.AddAttribute(3, "value", BindConverter.FormatValue(CurrentValue));
+						builder.AddAttribute(2, Html.CLASS, CssClass); // Overwrite Attributes["class"]
+						builder.AddAttribute(3, Html.VALUE, BindConverter.FormatValue(CurrentValue));
 
 						// Disabled?
-						if (Disabled ?? false) builder.AddAttribute(4, "disabled", string.Empty);
+						if (Disabled ?? false) builder.AddAttribute(4, Html.DISABLED, string.Empty);
 
 						// Help
-						if (HasHelp) builder.AddAttribute(5, "aria-describedby", $"{Id}-help");
+						if (HasHelp) builder.AddAttribute(5, Html.ARIA_DESCRIBEDBY, $"{Id}-help");
 
 						// OnChange
-						builder.AddAttribute(6, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+						builder.AddAttribute(6, Html.ONCHANGE, EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
 
-						// OnClick
-						if (ClearOnClick) builder.AddAttribute(7, "onclick", $"return textBox.clearValue('{Id}')");
-						else if (SelectOnClick) builder.AddAttribute(8, "onclick", $"return textBox.selectValue('{Id}')");
+						// Clear or Select OnClick
+						if (ClearOnClick)
+						{
+							builder.AddAttribute(7, Html.ONCLICK, EventCallback.Factory.Create(this, __value => CurrentValueAsString = string.Empty));
+						}
+						else if (SelectOnClick)
+						{
+							builder.AddAttribute(7, Html.ONCLICK, "this.select()");
+						}
 
 						// OnBlur
-						if (OnBlur.HasDelegate) builder.AddAttribute(9, "onblur", EventCallback.Factory.Create(this, this.OnBlurAsync));
+						if (OnBlur.HasDelegate) builder.AddAttribute(9, Html.ONBLUR, EventCallback.Factory.Create(this, this.OnBlurAsync));
 
 						builder.CloseElement();
 					}
@@ -48,26 +54,35 @@ namespace Blazorade.Bootstrap.Forms
 				case TextBoxMode.MultiLine:
 					{
 						// Input
-						builder.OpenElement(10, "textarea");
+						builder.OpenElement(10, Html.TEXTAREA);
 						builder.AddMultipleAttributes(11, this.Attributes);
-						builder.AddAttribute(12, "class", CssClass); // Overwrite Attributes["class"]
-						builder.AddAttribute(13, "value", BindConverter.FormatValue(CurrentValue));
+						builder.AddAttribute(12, Html.CLASS, CssClass); // Overwrite Attributes["class"]
+						builder.AddAttribute(13, Html.VALUE, BindConverter.FormatValue(CurrentValue));
 
 						// Disabled?
-						if (Disabled ?? false) builder.AddAttribute(14, "disabled", string.Empty);
+						if (Disabled ?? false) builder.AddAttribute(14, Html.DISABLED, string.Empty);
 
 						// Help
-						if (HasHelp) builder.AddAttribute(15, "aria-describedby", $"{Id}-help");
+						if (HasHelp) builder.AddAttribute(15, Html.ARIA_DESCRIBEDBY, $"{Id}-help");
 
 						// OnChange
-						builder.AddAttribute(16, "onchange", EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+						builder.AddAttribute(16, Html.ONCHANGE, EventCallback.Factory.CreateBinder<string>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
 
-						// OnClick
-						if (ClearOnClick) builder.AddAttribute(17, "onclick", $"return textBox.clearValue('{Id}')");
-						else if (SelectOnClick) builder.AddAttribute(18, "onclick", $"return textBox.selectValue('{Id}')");
+						// Clear or Select OnClick
+						if (ClearOnClick)
+						{
+							builder.AddAttribute(17, Html.ONCLICK, EventCallback.Factory.Create(this, __value => CurrentValueAsString = string.Empty));
+						}
+						else if (SelectOnClick)
+						{
+							builder.AddAttribute(17, Html.ONCLICK, "this.select()");
+						}
 
 						// OnBlur
-						if (OnBlur.HasDelegate) builder.AddAttribute(19, "onblur", EventCallback.Factory.Create(this, this.OnBlurAsync));
+						if (OnBlur.HasDelegate)
+						{
+							builder.AddAttribute(19, Html.ONBLUR, EventCallback.Factory.Create(this, this.OnBlurAsync));
+						}
 
 						builder.CloseElement();
 					}
